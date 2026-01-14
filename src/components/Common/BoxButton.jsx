7a97804c151ad4ef75160f696e015267e5f5541c
Jordan Grieve <grievejordan@yahoo.com>
2026-01-14 19:24:57 +0000

    Update BoxButton width to large and adjust CSS class for better alignment

--- src/components/Common/BoxButton.jsx
+++ src/components/Common/BoxButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function BoxButton() {
   return (
-    <button className="BoxButton w-md font-normal p-8 bg-red-700 mb-9 text-center items-center text-white">
+    <button className="BoxButton w-lg font-normal p-8 bg-red-700 mb-9 text-white">
       Contact Us
     </button>
   );
